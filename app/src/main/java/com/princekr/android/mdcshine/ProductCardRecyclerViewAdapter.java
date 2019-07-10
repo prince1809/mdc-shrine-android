f4b8654727b31ed0b29795b95525de5973782de4
prince <prince18pat@gmail.com>
2019-07-11 01:46:13 +0900

    add product card view and adapter

---
+++ app/src/main/java/com/princekr/android/mdcshine/ProductCardRecyclerViewAdapter.java
@@ -0,0 +1,10 @@
+package com.princekr.android.mdcshine;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ProductCardRecyclerViewAdapter extends RecyclerView.Adapter<ProductCardViewHolder> {
+
+   // private List<ProductEntry> pro
+}
---
+++ app/src/main/java/com/princekr/android/mdcshine/ProductCardViewHolder.java
@@ -0,0 +1,23 @@
+package com.princekr.android.mdcshine;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.toolbox.NetworkImageView;
+
+public class ProductCardViewHolder extends RecyclerView.ViewHolder {
+
+    public NetworkImageView productImage;
+    public TextView productTitle;
+    public TextView productPrice;
+
+    public ProductCardViewHolder(@NonNull View itemView) {
+        super(itemView);
+        productImage = itemView.findViewById(R.id.product_image);
+        productTitle = itemView.findViewById(R.id.product_price);
+        productPrice = itemView.findViewById(R.id.product_price);
+    }
+}
