c008d4572ceed29b5e9748f67d71dba0a4296574
prince <prince18pat@gmail.com>
2019-07-11 00:47:55 +0900

    add toolbar and attach it

--- app/src/main/java/com/princekr/android/mdcshine/ProductGridFragment.java
+++ app/src/main/java/com/princekr/android/mdcshine/ProductGridFragment.java
@@ -2,16 +2,45 @@ package com.princekr.android.mdcshine;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 public class ProductGridFragment extends Fragment {
 
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.shr_product_grid_fragment, container, false);
+        View view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false);
+        setupToolbar(view);
+        return view;
+    }
+
+
+    private void setupToolbar(View view) {
+        Toolbar toolbar = view.findViewById(R.id.app_bar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        if (activity != null) {
+            activity.setSupportActionBar(toolbar);
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+        menuInflater.inflate(R.menu.shr_toolbar_menu, menu);
+        super.onCreateOptionsMenu(menu, menuInflater);
     }
 }
